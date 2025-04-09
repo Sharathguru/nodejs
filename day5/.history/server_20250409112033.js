0d9b00daf8a361e2f02a010c93fdb7a99586fcf0
Sharathguru <vsarath200219@gmail.com>
2025-04-09 21:18:49 +0530

    day8

---
+++ day5/.history/server_20250409112033.js
@@ -0,0 +1,56 @@
+// import http from "http"
+// let server=http.createServer((req,res)=>
+// {
+//     res.write("hello world")
+//     res.end()
+// })
+
+// server.listen(5000,()=>
+// {
+//     console.log("server is an 5000......");
+    
+// })
+
+
+let http=require("http")
+let server=http.createServer((req,res)=>
+{
+    res.write("hello")
+    res.end()
+})
+server.listen(5000,()=>
+{
+    console.log("server 5000 -----");
+    
+})
+
+
+import { log } from "console"
+// et http=require("http")
+// let server=http.createServer((req,res)=>
+// {
+//     res.statusCode=200;
+//     res.statusMessage="okay"
+//     res.setHeader("content-type","text/plain")
+//     res.write("hello world")
+//     res.end()
+// })
+// server.listen(5000,()=>
+// {
+//     console.log("server 5000 -----");
+    
+// })
+
+
+
+import express from "express"
+
+let app=express()
+
+
+app.use()
+
+app.listen(3000,(req,res)=>{
+    console.log("serer");
+    
+})
